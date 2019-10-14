a843abfd02140e733aa459c04bf911fa06299e0a
Sami <smabp3@gmail.com>
2019-10-14 13:14:13 -0500

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoDashboardComponent } from './dashboard/video-dashboard/video-dashboard.component';
+import { VideoListComponent } from './dashboard/video-list/video-list.component';
+import { VideoPlayerComponent } from './dashboard/video-player/video-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoDashboardComponent,
+    VideoListComponent,
+    VideoPlayerComponent
   ],
   imports: [
     BrowserModule
